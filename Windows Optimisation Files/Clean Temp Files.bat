897400b5b5ff21b74427464aa7c204df359e4bc2
Trn44 <167228423+Trn44@users.noreply.github.com>
2025-04-18 17:51:55 +0100

    Update Clean Temp Files.bat

--- Windows Optimisation Files/Clean Temp Files.bat
+++ Windows Optimisation Files/Clean Temp Files.bat
@@ -8,7 +8,6 @@ set folder=%userprofile%\AppData\Local\Temp
 cd /d %folder%
 for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q) > NUL 2>&1
 
-
 echo Running Disk Cleanup
 set R_Key=HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches
 for %%k in (
@@ -27,7 +26,6 @@ for %%k in (
     reg add "%R_Key%\%%k" /v StateFlags0011 /t REG_DWORD /d 00000002 /f > NUL 2>&1
 )
 start /wait cleanmgr.exe /sagerun:11
-
 
 echo Clearing Event Logs
 for /F "tokens=*" %%G in ('wevtutil.exe el') DO (call :eventlog "%%G")
