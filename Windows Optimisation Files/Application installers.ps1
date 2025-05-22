14fed2949283f99c3c24303b453d07a1838cf5c4
Trn44 <jayden2002t@gmail.com>
2025-05-22 22:05:40 +0100

    Update Application installers.ps1

---
+++ Windows Optimisation Files/Application installers.ps1
@@ -0,0 +1,11 @@
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+    [Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Start-Process powershell.exe "-ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+    exit
+}
+
+$Host.UI.RawUI.BackgroundColor = "Black"
+Clear-Host
+
+function ApplicationInstallers {    
+}
