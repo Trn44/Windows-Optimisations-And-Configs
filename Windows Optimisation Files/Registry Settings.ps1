979dd76faece9a6b6e563fe56eccc2ba5058af86
Trn44 <jayden2002t@gmail.com>
2025-05-27 18:45:48 +0100

    Update Registry Settings.ps1

--- Windows Optimisation Files/Registry Settings.ps1
+++ Windows Optimisation Files/Registry Settings.ps1
@@ -25,7 +25,7 @@ Windows Registry Editor Version 5.00
 
 ; Windows Settings
 
-; Perfrmance Settings
+; Performance Settings
 
 ; Visual Settings
 
@@ -66,6 +66,41 @@ Windows Registry Editor Version 5.00
 function RevertRegistry {
     $RevertRegistry = @"
 Windows Registry Editor Version 5.00
+
+; Windows Settings
+
+; Performance Settings
+
+; Visual Settings
+
+; Application Settings
+
+; Telemetry & Spyware
+[HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager]
+"ContentDeliveryAllowed"=dword:00000001
+"FeatureManagementEnabled"=dword:00000001
+"OemPreInstalledAppsEnabled"=dword:00000001
+"PreInstalledAppsEnabled"=dword:00000001
+"PreInstalledAppsEverEnabled"=dword:00000001
+"RotatingLockScreenEnabled"=dword:00000001
+"RotatingLockScreenOverlayEnabled"=dword:00000001
+"SilentInstalledAppsEnabled"=dword:00000001
+"SlideshowEnabled"=dword:00000001
+"SoftLandingEnabled"=dword:00000001
+"SubscribedContent-310093Enabled"=-
+"SubscribedContent-314563Enabled"=-
+"SubscribedContent-338388Enabled"=-
+"SubscribedContent-338389Enabled"=-
+"SubscribedContent-338389Enabled"=-
+"SubscribedContent-338393Enabled"=-
+"SubscribedContent-338393Enabled"=-
+"SubscribedContent-353694Enabled"=-
+"SubscribedContent-353694Enabled"=-
+"SubscribedContent-353696Enabled"=-
+"SubscribedContent-353696Enabled"=-
+"SubscribedContent-353698Enabled"=-
+"SubscribedContentEnabled"=dword:00000001
+"SystemPaneSuggestionsEnabled"=dword:00000001
 
 "@
     Optimize-Registry -Content $RevertRegistry -FileName "RemoveOptimisations.reg"
