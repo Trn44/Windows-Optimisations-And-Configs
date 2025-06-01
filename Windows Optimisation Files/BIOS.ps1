c5a19566a025c9e6d4e01ee32a2095718471ed3f
Trn44 <jayden2002t@gmail.com>
2025-06-01 14:14:40 +0100

    Update BIOS.ps1

--- Windows Optimisation Files/BIOS.ps1
+++ Windows Optimisation Files/BIOS.ps1
@@ -6,3 +6,24 @@ if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
 
 $Host.UI.RawUI.BackgroundColor = "Black"
 Clear-Host
+
+function RestartToBIOS {
+	shutdown.exe /r /fw /t 8
+    Write-Host "`nRestarting to BIOS."
+    Start-Sleep -Seconds 5
+}
+
+do {
+    Clear-Host
+    Write-Host "1. Restart to BIOS"
+    Write-Host "2. Exit"
+    $Select = Read-Host "Enter 1 or 2"
+    switch ($Select) {
+        "1" {RestartToBIOS}
+        "2" {Exit}
+        default {
+            Write-Host "Invalid option, pick 1 or 2"
+            Start-Sleep -Seconds 2
+        }
+    }
+} while ($true)
