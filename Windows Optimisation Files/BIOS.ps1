8d66478285a4c0dd2504ccf608c5a0c558980d42
Trn44 <jayden2002t@gmail.com>
2025-06-02 14:37:53 +0100

    Update BIOS.ps1

--- Windows Optimisation Files/BIOS.ps1
+++ Windows Optimisation Files/BIOS.ps1
@@ -7,23 +7,34 @@ if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
 $Host.UI.RawUI.BackgroundColor = "Black"
 Clear-Host
 
+function MotherboardModel {
+    $Motherboard = Get-WmiObject Win32_BaseBoard | Select-Object -ExpandProperty Product
+    return $Motherboard
+}
+function MotherboardBIOS {
+    $MotherboardModel = MotherboardModel
+    $URL = "https://www.google.com/search?q=$MotherboardModel+BIOS"
+    Start-Process $URL
+}
 function RestartToBIOS {
-	shutdown.exe /r /fw /t 8
+    Shutdown.exe /r /fw /t 8
     Write-Host "`nRestarting to BIOS."
     Start-Sleep -Seconds 5
 }
 
 do {
     Clear-Host
-    Write-Host "1. Restart to BIOS"
-    Write-Host "2. Exit"
-    $Select = Read-Host "Enter 1 or 2"
+    Write-Host "1. Search for BIOS version update"
+    Write-Host "2. Restart to BIOS"
+    Write-Host "3. Exit"
+    $Select = Read-Host "Enter 1, 2 or 3"
     switch ($Select) {
-        "1" {RestartToBIOS}
-        "2" {Exit}
-        default {
-            Write-Host "Invalid option, pick 1 or 2"
-            Start-Sleep -Seconds 2
+        "1" {MotherboardBIOS}
+        "2" {RestartToBIOS}
+        "3" {Exit}
+    default {
+    Write-Host "Invalid option, pick 1 or 2"
+    Start-Sleep -Seconds 2
         }
     }
 } while ($true)
