59c660c889518a1e5cbe357038e32f30a8a99dd1
Trn44 <jayden2002t@gmail.com>
2025-06-04 15:59:19 +0100

    Update Application Installers.ps1

--- Windows Optimisation Files/Application installers.ps1
+++ Windows Optimisation Files/Application installers.ps1
@@ -7,17 +7,43 @@ if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
 $Host.UI.RawUI.BackgroundColor = "Black"
 Clear-Host
 
-function ApplicationInstallers {
-    Write-Host "1. Install ** "
-    Write-Host "2. Install ** "
-    Write-Host "1. Install ** "
-    Write-Host "2. Install ** "
-    Write-Host "1. Install ** "
-    Write-Host "2. Install ** "
-    Write-Host "1. Install ** "
-    Write-Host "2. Install ** "
-    Write-Host "1. Install ** "
-    Write-Host "2. Install ** "
-}
+function Install-VCRedistPackages {
+function Install-PlaceholderApp1 { Write-Host "Installing Placeholder"; Pause }
+function Install-PlaceholderApp2 { Write-Host "Installing Placeholder"; Pause }
+function Install-PlaceholderApp3 { Write-Host "Installing Placeholder"; Pause }
+function Install-PlaceholderApp4 { Write-Host "Installing Placeholder"; Pause }
+function Install-PlaceholderApp5 { Write-Host "Installing Placeholder"; Pause }
+function Install-PlaceholderApp6 { Write-Host "Installing Placeholder"; Pause }
+function Install-PlaceholderApp7 { Write-Host "Installing Placeholder"; Pause }
+function Install-PlaceholderApp8 { Write-Host "Installing Placeholder"; Pause }
 
-#Switch placeholders **
+do {
+    Clear-Host
+    Write-Host "1. Install C++ Redistributables"
+    Write-Host "2. Install Placeholder App"
+    Write-Host "3. Install Placeholder App"
+    Write-Host "4. Install Placeholder App"
+    Write-Host "5. Install Placeholder App"
+    Write-Host "6. Install Placeholder App"
+    Write-Host "7. Install Placeholder App"
+    Write-Host "8. Install Placeholder App"
+    Write-Host "9. Install Placeholder App"
+    Write-Host "10. Exit"
+    $Select = Read-Host "Enter 1 - 10"
+    switch ($Select) {
+        "1"  {Install-VCRedistPackages}
+        "2"  {Install-PlaceholderApp}
+        "3"  {Install-PlaceholderApp}
+        "4"  {Install-PlaceholderApp}
+        "5"  {Install-PlaceholderApp}
+        "6"  {Install-PlaceholderApp}
+        "7"  {Install-PlaceholderApp}
+        "8"  {Install-PlaceholderApp}
+        "9"  {Install-PlaceholderApp}
+        "10" {Exit}
+        default {
+            Write-Host "Invalid option, pick 1 - 10"
+            Start-Sleep -Seconds 2
+        }
+    }
+} while ($true)
