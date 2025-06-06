a15cedb39c29d2d9517604533797e578232e1ae0
Trn44 <jayden2002t@gmail.com>
2025-06-06 17:03:48 +0100

    Add Discord Debloat.ps1

---
+++ Windows Optimisation Files/Extras/Discord Debloat.ps1
@@ -0,0 +1,15 @@
+$Host.UI.RawUI.BackgroundColor = "Black"
+Clear-Host
+
+Write-Host "Debloating Discord."
+Get-Process -Name "Discord" -ErrorAction SilentlyContinue | Stop-Process -Force
+
+$VersionFolder = Get-ChildItem "$env:LOCALAPPDATA\Discord\app-*" -Directory | Sort-Object Name -Descending | Select-Object -First 1
+$KeepModules = @("discord_desktop_core", "discord_krisp", "discord_rpc", "discord_utils", "discord_voice")
+Get-ChildItem "$($VersionFolder.FullName)\modules" -Directory | Where-Object {
+    $Modules = ($_).Name.Split('-')[0] 
+    $Modules -notin $KeepModules } | Remove-Item -Recurse -Force
+Get-ChildItem "$($VersionFolder.FullName)\locales" -File | Where-Object { $_.Name -ne "en-GB.pak" } | Remove-Item -Force
+
+Write-Host "`nDiscord debloated."
+Start-Sleep -Seconds 4
