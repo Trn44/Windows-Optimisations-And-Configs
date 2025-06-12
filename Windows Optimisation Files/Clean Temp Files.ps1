0862a24315965fffad0b3a4d5cc5ce6f494e688c
Trn44 <jayden2002t@gmail.com>
2025-06-13 00:24:55 +0100

    Update Clean Temp Files.ps1.ps1

--- Windows Optimisation Files/Clean Temp Files.ps1
+++ Windows Optimisation Files/Clean Temp Files.ps1
@@ -7,30 +7,13 @@ if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
 $Host.UI.RawUI.BackgroundColor = "Black"
 Clear-Host
 
-Write-Output "Running Windows Disk Cleanup"
+Write-Output "Running Windows Disk Cleanup and cleaning temporary files."
 Start-Process cleanmgr.exe
 
-Write-Output "Cleaning C:\Windows\Temp folder"
-$WinTemp = "C:\Windows\Temp"
-Get-ChildItem -Path $WinTemp -Force -Recurse -ErrorAction SilentlyContinue | ForEach-Object {
-    $File = $_.FullName
-    Try {
-        Remove-Item -Path $File -Recurse -Force -ErrorAction Stop
-        Write-Output "Deleted: $File"
-    } Catch {
-        Write-Output "$_"
-    }
-}
+$TempFIles = ("C:\Windows\Temp", "$env:LOCALAPPDATA\Temp")
 
-Write-Output "Cleaning C:\Users\$env:USERNAME\AppData\Local\Temp folder"
-$UserTemp = "$env:LOCALAPPDATA\Temp"
-Get-ChildItem -Path $UserTemp -Force -Recurse -ErrorAction SilentlyContinue | ForEach-Object {
-    $File = $_.FullName
-    Try {
-        Remove-Item -Path $File -Recurse -Force -ErrorAction Stop
-        Write-Output "Deleted: $File"
-    } Catch {
-        Write-Output "$_"
-    }
+foreach ($Directory in $TempFIles) {
+    Get-ChildItem -Path $Directory -Force -Recurse -ErrorAction SilentlyContinue | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue
 }
-Read-Host "`nPress Enter to exit"
+Write-Output "`nTemp files cleaned."
+Start-Sleep -Seconds 5
