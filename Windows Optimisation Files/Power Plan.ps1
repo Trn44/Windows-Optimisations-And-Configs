66af5f8b1280ab9696b58a455b504d8c31916264
Trn44 <jayden2002t@gmail.com>
2025-06-22 00:16:39 +0100

    Update multiple files

--- Windows Optimisation Files/Power Plan.ps1
+++ Windows Optimisation Files/Power Plan.ps1
@@ -1,8 +1,5 @@
-if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
-    [Security.Principal.WindowsBuiltInRole] "Administrator")) {
-    Start-Process powershell.exe "-ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
-    exit
-}
+if (-not ([Security.Principal.WindowsIdentity]::GetCurrent().IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))) 
+{ Start-Process powershell.exe "-ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
 $Host.UI.RawUI.BackgroundColor = "Black"
 Clear-Host
