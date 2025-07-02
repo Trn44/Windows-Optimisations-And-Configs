0e92180502d8402dcdba5c87d6cc34201efbe6fb
Trn44 <jayden2002t@gmail.com>
2025-07-02 16:15:08 +0100

    Refactor Power Plan script settings and improve user options

--- Windows Optimisation Files/Power Plan.ps1
+++ Windows Optimisation Files/Power Plan.ps1
@@ -4,14 +4,35 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 $Host.UI.RawUI.BackgroundColor = "Black"
 Clear-Host
 
-cmd /c "powercfg /duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61 99999999-9999-9999-9999-999999999999 >nul 2>&1"
-cmd /c "powercfg /SETACTIVE 99999999-9999-9999-9999-999999999999 >nul 2>&1"
+function Powerplan {
+
+cmd /c "powercfg /duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61 44444444-4444-4444-4444-444444444444 >nul 2>&1"
+cmd /c "powercfg /SETACTIVE 44444444-4444-4444-4444-444444444444 >nul 2>&1"
+powercfg /hibernate off
+cmd /c "reg add `"HKLM\SYSTEM\CurrentControlSet\Control\Power`" /v `"HibernateEnabled`" /t REG_DWORD /d `"0`" /f >nul 2>&1"
+cmd /c "reg add `"HKLM\SYSTEM\CurrentControlSet\Control\Power`" /v `"HibernateEnabledDefault`" /t REG_DWORD /d `"0`" /f >nul 2>&1"
+# Lock disable
+cmd /c "reg add `"HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\FlyoutMenuSettings`" /v `"ShowLockOption`" /t REG_DWORD /d `"0`" /f >nul 2>&1"
+# Sleep disable
+cmd /c "reg add `"HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FlyoutMenuSettings`" /v `"ShowSleepOption`" /t REG_DWORD /d `"0`" /f >nul 2>&1"
+# Fast boot disable
+cmd /c "reg add `"HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Power`" /v `"HiberbootEnabled`" /t REG_DWORD /d `"0`" /f >nul 2>&1"
+# CPU core unparking
+cmd /c "reg add `"HKLM\SYSTEM\ControlSet001\Control\Power\PowerSettings\54533251-82be-4824-96c1-47b60b740d00\0cc5b647-c1df-4637-891a-dec35c318583`" /v `"ValueMax`" /t REG_DWORD /d `"0`" /f >nul 2>&1"
+# Power throttling disable
+cmd /c "reg add `"HKLM\SYSTEM\CurrentControlSet\Control\Power\PowerThrottling`" /v `"PowerThrottlingOff`" /t REG_DWORD /d `"1`" /f >nul 2>&1"
+# Unhide hub selective suspend timeout
+cmd /c "reg add `"HKLM\System\ControlSet001\Control\Power\PowerSettings\2a737441-1930-4402-8d77-b2bebba308a3\0853a681-27c8-4100-a2fd-82013e970683`" /v `"Attributes`" /t REG_DWORD /d `"2`" /f >nul 2>&1"
+# Unhide USB 3 link power management
+cmd /c "reg add `"HKLM\System\ControlSet001\Control\Power\PowerSettings\2a737441-1930-4402-8d77-b2bebba308a3\d4e98f31-5ffe-4ce1-be31-1b38b384c009`" /v `"Attributes`" /t REG_DWORD /d `"2`" /f >nul 2>&1"
+
+}
 
 
 do {
     Clear-Host
     Write-Host "1. Set Powerplan"
-    Write-Host "2. Restart to default Powerplan"
+    Write-Host "2. Revert to default Powerplan"
     Write-Host "`nEsc to Exit"
    $KeyPress = [System.Console]::ReadKey($true)
     switch ($KeyPress.Key) {
