96d63f45a63a4b3c255cd8205e54ab3d489dd076
Trn44 <jayden2002t@gmail.com>
2025-09-29 15:55:31 +0100

    Add Windows Activation script

---
+++ Windows Optimisation Files/Extras/Windows Activation.ps1
@@ -0,0 +1,5 @@
+$Host.UI.RawUI.BackgroundColor = "Black"
+Clear-Host
+
+Write-Host "Credits: Massgravel(https://github.com/massgravel/Microsoft-Activation-Scripts)" -ForegroundColor Green
+irm https://get.activated.win | iex
