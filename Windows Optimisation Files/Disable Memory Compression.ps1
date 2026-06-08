0e7904bc52f0a8b59b6bccdd4bded1a235995ac5
Trn44 <trn44@proton.me>
2026-06-08 21:06:32 +0100

    Enhance memory compression script with user prompts and exit options

--- Windows Optimisation Files/Disable Memory Compression.ps1
+++ Windows Optimisation Files/Disable Memory Compression.ps1
@@ -5,9 +5,35 @@ $Host.UI.RawUI.WindowTitle = "Disable Memory Compression"
 $Host.UI.RawUI.BackgroundColor = "Black"
 Clear-Host
 
+Write-Host "Windows 11 Default = Enabled. Not recommended to disable if you have less than 16GB RAM."
+
+function Disable {
 Write-Host "Disabling Memory Compression"
 Disable-MMAgent -MemoryCompression -ErrorAction SilentlyContinue | Out-Null
 Start-Sleep -Seconds 5
 Get-MMAgent | Format-List -Property MemoryCompression
-Write-Host "`nPress any key to exit"
-$Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+Start-Sleep -Seconds 5
+}
+  
+
+function Enable {
+Write-Host "Enabling Memory Compression"
+Enable-MMAgent -MemoryCompression -ErrorAction SilentlyContinue | Out-Null
+Start-Sleep -Seconds 5
+Get-MMAgent | Format-List -Property MemoryCompression
+Start-Sleep -Seconds 5
+}
+
+do {
+    Clear-Host
+    Write-Host "1. Disable Memory Compression"
+    Write-Host "2. Enable Memory Compression"
+    Write-Host "`nEsc to Exit"
+   $KeyPress = [System.Console]::ReadKey($true)
+    switch ($KeyPress.Key) {
+        'D1' {Disable}
+        'D2' {Enable}
+        'Escape' {Exit}
+        default { Write-Host "`nInvalid option, pick 1, 2 or Esc to exit"; Start-Sleep -Seconds 2}
+    }
+} while ($true)
